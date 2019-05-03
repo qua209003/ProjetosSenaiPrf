7f2f2f45362f75ef762d5c60760976d380832273
luiz.felipe <luiz.felipe@LAB-F05-28.academico.local>
2019-05-03 19:53:09 -0300

    Reenviando o projeto Banco

---
+++ workspace/ExmplBancoPrf/src/ExemploReferencia.java
@@ -0,0 +1,27 @@
+
+
+public class ExemploReferencia {
+
+	public static void main(String[] args) {
+		// ler página 55 da apostila
+		
+		Conta c1 = new Conta(new Cliente("Luiz",0));
+		c1.depositar(100);
+		
+		Conta c2 = c1;
+		
+		c2.depositar(100);
+		
+		c2 = new Conta(new Cliente("Felipe",0));
+		c2.depositar(50);
+		
+		System.out.println(c1);
+		System.out.println(c1.getSaldo());
+		
+		
+		System.out.println(c2);
+		System.out.println(c2.getSaldo());
+
+	}
+
+}
