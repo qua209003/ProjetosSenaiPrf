3db584a66b5aaab5bcb8cc86e9d6ea438a76e5f0
luiz.felipe <luiz.felipe@LAB-F05-28.academico.local>
2019-07-09 19:21:06 -0300

    Atualizações JSF gravando informações no banco de dados e mostrando Mensagem.

--- workspace/PrimeiroJSF/src/org/senai/exemplojsf/dados/ClienteDAO.java
+++ workspace/PrimeiroJSF/src/org/senai/exemplojsf/dados/ClienteDAO.java
@@ -1,7 +1,41 @@
 package org.senai.exemplojsf.dados;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.senai.exemplojsf.modelo.Cliente;
+
 public class ClienteDAO {
-	public void incluir() {
+	
+	public boolean incluir(Cliente objCliente) {
+Connection conexao = new ConectarJDBC().getConectar();
 		
+		if(conexao != null) {
+			String sql = "insert into cliente(" + 
+					"	nome  ," + 
+					"	email         ," + 
+					"	senha          )" + 
+					"values (?," + 
+					"		?," + 
+					"		?" + 
+					"		)";
+			try {
+				PreparedStatement prepararSQL =
+						conexao.prepareStatement(sql);
+				prepararSQL.setString(1, objCliente.getNome());
+				prepararSQL.setString(2, objCliente.getEmail());
+				prepararSQL.setString(3, objCliente.getSenha());
+				
+				prepararSQL.execute();
+				prepararSQL.close();
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return false;
+			}
+			
+		}
+		return false;
 	}
 }
--- workspace/PrimeiroJSF/src/org/senai/exemplojsf/managedbean/ClienteMB.java
+++ workspace/PrimeiroJSF/src/org/senai/exemplojsf/managedbean/ClienteMB.java
@@ -1,11 +1,14 @@
 package org.senai.exemplojsf.managedbean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
+import org.senai.exemplojsf.dados.ClienteDAO;
 import org.senai.exemplojsf.modelo.Cliente;
 @ManagedBean
 public class ClienteMB {
-	private Cliente cliente;
+	private Cliente cliente = new Cliente();
 
 	public Cliente getCliente() {
 		return cliente;
@@ -16,6 +19,22 @@ public class ClienteMB {
 	}
 	
 	public void gravar() {
+		ClienteDAO dao = new ClienteDAO();
+		String msg = "";
 		
+		if(dao.incluir(cliente)) {
+			msg = "Sucesso ao gravar";
+		}else {
+			msg = "Erro ao gravar";
+		}
+		
+		// Exercício 
+		// se houver sucesso ao gravar 
+		// mostre mensagem positiva
+		// senão mensagem negativa
+		
+		
+		FacesContext.getCurrentInstance().addMessage(
+				null, new FacesMessage(msg));
 	}
 }
