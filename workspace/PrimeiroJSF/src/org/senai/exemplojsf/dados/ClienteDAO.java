30830560ea8035dcac3d38f6071f187ae1045d83
luiz.felipe <luiz.felipe@LAB-F05-28.academico.local>
2019-07-10 19:18:46 -0300

    Modificações para preparar a edição

--- workspace/PrimeiroJSF/src/org/senai/exemplojsf/dados/ClienteDAO.java
+++ workspace/PrimeiroJSF/src/org/senai/exemplojsf/dados/ClienteDAO.java
@@ -2,31 +2,69 @@ package org.senai.exemplojsf.dados;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.senai.exemplojsf.modelo.Cliente;
 
 public class ClienteDAO {
 	
-	public boolean incluir(Cliente objCliente) {
-Connection conexao = new ConectarJDBC().getConectar();
+	
+	public List<Cliente> listaCliente() {
 		
-		if(conexao != null) {
-			String sql = "insert into cliente(" + 
-					"	nome  ," + 
-					"	email         ," + 
-					"	senha          )" + 
-					"values (?," + 
-					"		?," + 
-					"		?" + 
-					"		)";
+		Connection conexao = new ConectarJDBC().getConectar();
+
+		if (conexao != null) {
+			String sql = " select * from cliente ";
 			try {
-				PreparedStatement prepararSQL =
-						conexao.prepareStatement(sql);
+				PreparedStatement prepararSQL = conexao.prepareStatement(sql);
+				ResultSet dadosDoBanco = prepararSQL.executeQuery();
+				
+				//objeto de retorno da lista dos cliente 
+				// que estão no bando de dados
+				List<Cliente> lsClientes = new ArrayList<Cliente>();
+				
+				while(dadosDoBanco.next()) {
+					Cliente objCliente = new Cliente();
+					
+					objCliente.setNome (dadosDoBanco.getString("nome"));
+					objCliente.setEmail(dadosDoBanco.getString("email"));
+					objCliente.setSenha(dadosDoBanco.getString("senha"));
+					
+					lsClientes.add(objCliente);
+				}
+				// para selecionar linhas e colunas 
+				// usar  alt+shift+a
+				
+				
+				prepararSQL.close();
+				return lsClientes;
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+
+		}
+
+		
+		return null;
+	}
+	
+
+	public boolean incluir(Cliente objCliente) {
+		Connection conexao = new ConectarJDBC().getConectar();
+
+		if (conexao != null) {
+			String sql = "insert into cliente(" + "	nome  ," + "	email         ," + "	senha          )"
+					+ "values (?," + "		?," + "		?" + "		)";
+			try {
+				PreparedStatement prepararSQL = conexao.prepareStatement(sql);
 				prepararSQL.setString(1, objCliente.getNome());
 				prepararSQL.setString(2, objCliente.getEmail());
 				prepararSQL.setString(3, objCliente.getSenha());
-				
+
 				prepararSQL.execute();
 				prepararSQL.close();
 				return true;
@@ -34,7 +72,7 @@ Connection conexao = new ConectarJDBC().getConectar();
 				e.printStackTrace();
 				return false;
 			}
-			
+
 		}
 		return false;
 	}
--- workspace/PrimeiroJSF/src/org/senai/exemplojsf/managedbean/ClienteMB.java
+++ workspace/PrimeiroJSF/src/org/senai/exemplojsf/managedbean/ClienteMB.java
@@ -1,13 +1,17 @@
 package org.senai.exemplojsf.managedbean;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
 import org.senai.exemplojsf.dados.ClienteDAO;
 import org.senai.exemplojsf.modelo.Cliente;
+
 @ManagedBean
 public class ClienteMB {
+
 	private Cliente cliente = new Cliente();
 
 	public Cliente getCliente() {
@@ -17,24 +21,41 @@ public class ClienteMB {
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-	
+
+	public List<Cliente> lsCliente() {
+		ClienteDAO dao = new ClienteDAO();
+		return dao.listaCliente();
+	}
+
+	public void editar(Cliente obj) {
+
+		System.out.println(obj.getNome());
+
+		FacesContext.getCurrentInstance()
+		.getExternalContext()
+		.getSessionMap()
+		.put("editar", obj);
+		// esse comandos fazem com que
+		// o objeto cliente selecionado para
+		// ser editado fique na sessão
+	}
+
 	public void gravar() {
 		ClienteDAO dao = new ClienteDAO();
 		String msg = "";
-		
-		if(dao.incluir(cliente)) {
+
+		if (dao.incluir(cliente)) {
 			msg = "Sucesso ao gravar";
-		}else {
+		} else {
 			msg = "Erro ao gravar";
 		}
-		
-		// Exercício 
-		// se houver sucesso ao gravar 
+
+		// Exercício
+		// se houver sucesso ao gravar
 		// mostre mensagem positiva
 		// senão mensagem negativa
-		
-		
-		FacesContext.getCurrentInstance().addMessage(
-				null, new FacesMessage(msg));
+
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(msg));
 	}
+
 }
